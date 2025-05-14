919ab3a024649a79a9d91646a29b776c427206c9
S1N <151928881+sintxcs@users.noreply.github.com>
2025-05-14 12:56:51 +0800

    Add files via upload

---
+++ credits.js
@@ -0,0 +1,10 @@
+/* credits.js
+
+An api using the post and get methods for handling the endpoint as a command from the Python tool.
+
+It's about handling the credit key for the user to access the checker.sin
+
+
+--- not for open source.
+
+*/
---
+++ server.js
@@ -0,0 +1,8 @@
+/* server.js
+
+An api using the post method to call the main server for extracting a valid account for the checker.sin script
+
+
+-- not for open source
+
+*/
